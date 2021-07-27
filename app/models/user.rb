5be72380e58362125ebf80eb206ca5b263da4c8b
Amber Kolar <amberj.kolar@gmail.com>
2021-07-26 19:37:52 -0700

    Create user model References #1

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ApplicationRecord
-  has_many :characters
+
+  has_secure_password
+
+  has_many :characters, dependent: :destroy
+
+  validates :username, presence: true, uniqueness: {case_sensitive: false}
+
 end
