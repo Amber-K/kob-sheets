b817e4910b65d892e26f0fec4305cb035ac2898e
Amber Kolar <amberj.kolar@gmail.com>
2021-07-26 19:55:49 -0700

    Migrate db to have correct characters table References #2

--- db/migrate/20210720055421_create_characters.rb
+++ db/migrate/20210720055421_create_characters.rb
@@ -1,8 +1,8 @@
 class CreateCharacters < ActiveRecord::Migration[6.1]
   def change
     create_table :characters do |t|
-      t.bigint :user_id, null: false
       t.string :name, default: ""
+      t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end
