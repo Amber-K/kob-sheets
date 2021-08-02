55ff9b41bb1bbfce275c4fe50a5f42e2883d73ee
Amber Kolar <amberj.kolar@gmail.com>
2021-08-02 04:11:54 -0700

    Make controller let new character values be accessed References #5

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -48,7 +48,7 @@ class CharactersController < ApplicationController
   private
 
   def character_params
-    params.require(:character).permit(:name, :user_id)
+    params.require(:character).permit(:name, :user_id, :allegiance, :class_option, :level, :current_health, :age, :sex, :height, :weight, :body_type, :skin, :race_ethnicity, :eyes, :hair, :distinguishing_features, :outfit, :carries, :species, :family_type, :family_description, :occupation_type, :occupation_description, :backstory_type, :backstory_description, :likes, :dislikes, :hobby_type, :hobby_description, :habit_type, :habit_description, :fear_type, :fear_description, :quirk_type, :quirk_description, :base_athleticism, :base_coordination, :base_drive, :base_knowledge, :base_openness, :base_sociability, :base_stability, :base_destroy, :base_grip, :base_leap, :base_run, :base_throw, :base_balance, :base_evade, :base_maneuver, :base_sneak, :base_create, :base_endure, :base_regulate, :base_debate, :base_decipher, :base_diagnose, :base_infer, :base_recall, :base_improvise, :base_notice, :base_impress, :base_lead, :base_lie, :base_please, :base_empathize, :base_soothe, :base_specialties)
   end
 
 end
