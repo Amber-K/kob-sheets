bbc7f6c0af9c7f2b7e4bb653789b2e2a05ae3334
Amber Kolar <amberj.kolar@gmail.com>
2021-08-02 22:44:07 -0700

    Add inventory to characters References #5

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_02_210232) do
+ActiveRecord::Schema.define(version: 2021_08_03_053620) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -87,6 +87,7 @@ ActiveRecord::Schema.define(version: 2021_08_02_210232) do
     t.integer "base_soothe"
     t.text "base_specialties"
     t.string "image_url"
+    t.text "inventory"
     t.index ["user_id"], name: "index_characters_on_user_id"
   end
 
