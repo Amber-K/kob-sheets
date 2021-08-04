ffdb0bfd6b54850df7604bdce41b94e7e96f4bae
Amber Kolar <amberj.kolar@gmail.com>
2021-08-04 01:55:40 -0700

    Add regex validation to make sure image urls look right References #7

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,7 @@
 class Character < ApplicationRecord
   belongs_to :user
+  validates :image_url, format: {
+    with: /\A(https?:\/\/.+\..+\/.+\.((jpe?|pn|sv)g|webp))?\z/i,
+    message: "must be a link to a still image"
+  }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,5 @@
 class User < ApplicationRecord
-
   has_secure_password
-
   has_many :characters, dependent: :destroy
-
   validates :username, presence: true, uniqueness: {case_sensitive: false}
-
 end
